8cb4f86a6106a3d9d99ac7cbebda000428f61059
Ramadan95 <kagermanov-r@mail.ru>
2020-06-18 22:15:00 +0300

    added a solution to the program Running Sum of 1d Array

---
+++ src/com/ramazan/lite/RunningSumOf1dArray/Solution.java
@@ -0,0 +1,18 @@
+package com.ramazan.lite.RunningSumOf1dArray;
+
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(runningSum(new int[]{3,1,2,10,1})));
+    }
+
+    public static int[] runningSum(int[] nums) {
+        int temp = 0;
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] + temp;
+            temp = nums[i];
+        }
+        return nums;
+    }
+}
